bbdd41c5243977a1c74167487a8f00687fb6f199
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-10-03 14:48:29 -0300

    Subindo commit

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { Elysia } from "elysia";
+import { z } from "zod";
+import { openapi } from "@elysiajs/openapi";
+import { betterAuthPlugin } from "./http/plugins/better-auth";
+import { OpenAPI } from "./auth";
+
+const app = new Elysia()
+  .use(
+    openapi({
+      documentation: {
+        components: await OpenAPI.components,
+        paths: await OpenAPI.getPaths(),
+      },
+    })
+  )
+  .use(betterAuthPlugin)
+  .get("/", () => "Hello Elysia")
+  .get(
+    "/users/:id",
+    ({ params, user }) => {
+      const userId = params.id;
+      const auhenticatedUserName = user.name;
+      console.log({ auhenticatedUserName });
+      return { id: userId, name: "Andrey Onoue" };
+    },
+    {
+      auth: true,
+      detail: {
+        summary: "Buscar um usuário por ID",
+        tags: ["users"],
+      },
+      params: z.object({
+        id: z.string(),
+      }),
+      response: {
+        200: z.object({
+          id: z.string(),
+          name: z.string(),
+        }),
+      },
+    }
+  )
+  .listen(3333);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
